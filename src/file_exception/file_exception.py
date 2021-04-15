46234f2f6ce2e6ef76d94d2b2978f2ae6c81f3cf
laxmitimalsina <65842436+laxmitimalsina@users.noreply.github.com>
2021-04-15 14:58:41 +0200

    Update file_exception.py

--- src/file_exception/file_exception.py
+++ src/file_exception/file_exception.py
@@ -2,6 +2,7 @@ import sys
 
 
 class FileExceptions(Exception):
+    """Catch Invalid file and gives exception """"
     def __init__(self, path, message=None):
         if message is None:
             message = ""
@@ -11,16 +12,19 @@ class FileExceptions(Exception):
 
     @property
     def show(self):
+        """show the error message """
         full_message = self.message + ": " + self.path
         print(full_message, file=sys.stderr)
         return full_message
 
 
 class InvalidFilePath(FileExceptions, FileNotFoundError):
+    """catch invalid file path"""
     def __init__(self, path, msg="File path not found"):
         super(InvalidFilePath, self).__init__(path, msg)
 
 
 class InvalidFile(FileExceptions):
+    """catch invalid file path"""
     def __init__(self, path, msg="Invalid file extension"):
         super(InvalidFile, self).__init__(path, msg)
